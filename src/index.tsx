16a771aec599fd0d756fa229b3b69fdbdc8129b9
Eric Dahlgren <eric@dgren.dev>
2022-05-23 12:59:40 +0200

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import 'react-native-gesture-handler';
+import {Text, ThemeProvider} from 'ui';
+import FlashMessage from 'react-native-flash-message';
+import {RootNavigator} from 'navigation';
+import {hydrateAuth, setI18nConfig} from 'core';
+import APIProvider from 'api/APIProvider';
+
+setI18nConfig();
+hydrateAuth();
+
+const App = () => {
+  return (
+    <APIProvider>
+      <ThemeProvider>
+        <Text>Hello</Text>
+        <RootNavigator />
+        <FlashMessage position="top" />
+      </ThemeProvider>
+    </APIProvider>
+  );
+};
+
+export default App;
